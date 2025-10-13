131c0b5de38120f710e5c18f23873f5c59b14710
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-10-13 18:53:40 +0200

    required changes

--- scripts/set_zenoh_config.sh
+++ scripts/set_zenoh_config.sh
@@ -1,4 +1,4 @@
 export ROS_DOMAIN_ID=100
 export RMW_IMPLEMENTATION=rmw_zenoh_cpp
-export ZENOH_ROUTER_CONFIG_URI=$HOME/ros2_ws/src/crisp_controllers_demos/zenoh_router_config.json5
+export ZENOH_ROUTER_CONFIG_URI=$HOME/ros2_ws/src/aloha4franka/zenoh_router_config.json5
 ros2 daemon stop && ros2 daemon start
--- scripts/setup_middleware.sh
+++ scripts/setup_middleware.sh
@@ -10,11 +10,11 @@ echo "Setting up ROS 2 middleware with '$RMW'..."
 case "$RMW" in
     "cyclone")
         echo "Setting up CycloneDDS middleware..."
-        source src/crisp_controllers_demos/scripts/set_cyclone_config.sh
+        source src/aloha4franka/scripts/set_cyclone_config.sh
         ;;
     "zenoh")
         echo "Setting up Zenoh middleware..."
-        source src/crisp_controllers_demos/scripts/set_zenoh_config.sh
+        source src/aloha4franka/scripts/set_zenoh_config.sh
         ;;
     "fastdds"|"fast"|"")
         echo "Using default FastDDS middleware"
